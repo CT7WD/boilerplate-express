9b905bb9a15904ea3010ec5842b8e516792c8f94
Gerald Whitehead <ctwebdev7@gmail.com>
2024-04-24 23:22:10 +0000

    static json reqeuest

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 let express = require('express');
 let app = express();
 
+/*console.log("Hello World")
 
+app.use("/public", express.static( __dirname + "/public"));
+
+app.get("/", function(req, res) {
+    // res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
+});*/
+
+app.get("/json", (req, res) => {
+    res.json({ message: "Hello json" });
+});
 
 
 
